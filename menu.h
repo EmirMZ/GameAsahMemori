cb996511cc689310ae3979390fe2eddcc5974949
Emir Muhamad Zaid <75107919+Zocom59@users.noreply.github.com>
2021-06-16 10:09:30 +0700

    menu.h added

---
+++ menu.h
@@ -0,0 +1,5 @@
+int menu(){
+	
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),BACKGROUND_BLUE|BACKGROUND_RED|BACKGROUND_INTENSITY);
+    printf("\n\nStudytonight just showed you how to put colors to your code!!");
+}
