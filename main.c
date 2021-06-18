b2b0b5ce23c1aac4522019a55b93010869578be1
qintharaandini <70939623+qintharaandini@users.noreply.github.com>
2021-06-18 21:06:29 +0700

    :(

--- main.c
+++ main.c
@@ -8,6 +8,26 @@
 
 int main(){
 	//enableVT();
-	menu(0);
+	//menu(0);
 	//game();
+	
+	int num;
+    FILE *fptr;
+    
+    fptr = fopen("program.txt","w+");
+	
+	if(fptr == NULL)
+    {
+      printf("Error!");   
+      exit(1);             
+    }
+    
+     printf("Enter num: ");
+   scanf("%d",&num);
+
+   fprintf(fptr,"%d",num);
+   printf("Value of n= %d", num);
+   
+   fclose(fptr);
+
 }
