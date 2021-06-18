2a69498c9961501c4d1f2152f9d38e1c5e086caf
qintharaandini <70939623+qintharaandini@users.noreply.github.com>
2021-06-18 17:22:55 +0700

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <windows.h>
+
 void red () {
   printf("\033[1;31m");
 }
@@ -14,33 +19,64 @@ void reset () {
   printf("\033[0m");
 }
 
+void menuMain (int selected){
+	int i;
+	char menu [9][5][32];
+	strcpy (menu[0][0], "Play");
+	strcpy (menu[0][1], "Help");
+	strcpy (menu[0][2], "Score");
+	strcpy (menu[0][3], "Exit");
+	strcpy (menu[1][0], "Play");
+	strcpy (menu[1][0], "Play");
+	strcpy (menu[1][0], "Play");
+	strcpy (menu[0][0], "Play");
+	
+	for (i = 0; i < 5; i++)
+	{
+		if (selected == i)
+		{
+			red();
+			printf("%s\n", menu[0][i]);
+			reset();
+		}
+		else
+		{
+			printf("%s\n", menu[0][i]);
+		}
+	}
+}
+
 
 int menu(){
 	system("cls");
 	red ();
     printf("test");
     reset ();
+    int selection, i = 1;
 
 	while(1){
+		if (selection == 5){
+			selection = 1;
+		}
+		else if (selection < 1){
+			selection = 5;
+		}
 		if (getch() == 224){ //224 is Escape
     	switch(getch()) {
     		case 72:
     			system("cls");
-    			green ();
-    			printf("up arrow");
-    			reset ();
+    			selection --;
 				break;
 			case 80:
 				system("cls");
-				red ();
-				printf("down arrow");
-				reset ();
+				selection ++;
+				break;
+			default :
 				break;
 		}
-	}
-	    
 		
-	
+		menuMain(selection);
+	}    
     
 
 	}
