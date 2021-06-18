f08e37f6a4d9e16f87e16da0cdbb2b3cfe82b427
qintharaandini <70939623+qintharaandini@users.noreply.github.com>
2021-06-18 18:35:50 +0700

    :(

---
+++ help.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "graphics."
+
+int main(){
+	textcolor(3);
+	
+	printf("============================= HELP =============================");
+	cprintf("\nCara Kerja game ini adalah sebagai berikut :\n");
+	printf("\n1. Masukkan nama dan tingkat kesulitan (Difficulty) yang ingin anda mainkan\n2. Ingatlah urutan warna yang akan ditampilkan pada layar anda");
+	printf("\n3. Masukkan jawaban dari urutan warna yang telah dilihat \n   -> Jika anda benar maka anda masuk ke level berikutnya");
+	printf("\n   -> Jika anda kalah, maka permainan akan berakhir dan menampilkan score anda");
+	printf("\n4. Jika anda ingin melihat score yang ada, maka pilihlah option SCORE");
+	printf("\n5. Untuk keluar dari program, pilihlah option EXIT");
+	
+	printf("\n\npress any key to go back to menu...");
+	getch();
+	
+	return 0;
+}
